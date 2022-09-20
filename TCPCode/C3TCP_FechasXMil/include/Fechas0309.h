026449401b1c142d74635880b6b85bf42487eb45
florencialicarzi <florencia.licarzi13@gmail.com>
2022-09-20 16:17:26 -0300

    add C3TCP

---
+++ TCPCode/C3TCP_FechasXMil/include/Fechas0309.h
@@ -0,0 +1,15 @@
+#ifndef FECHAS0309_H
+#define FECHAS0309_H
+
+
+class Fechas0309
+{
+    public:
+        Fechas0309();
+
+    protected:
+
+    private:
+};
+
+#endif // FECHAS0309_H
---
+++ TCPCode/C3TCP_FechasXMil/src/Fechas0309.cpp
@@ -0,0 +1,6 @@
+#include "Fechas0309.h"
+
+Fechas0309::Fechas0309()
+{
+    //ctor
+}
