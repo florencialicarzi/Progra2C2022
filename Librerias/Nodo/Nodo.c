17eb10c80d4c332d556b0ab9151e38e62b0b0964
Florencia Licarzi <37675631+florencialicarzi@users.noreply.github.com>
2022-10-24 10:21:55 -0300

    Add Lib Nodo

---
+++ Librerias/Nodo/Nodo.c
@@ -0,0 +1,26 @@
+Nodo* crearNodo(const void* elem, size_t tamElem)
+{
+    Nodo* nue = malloc(sizeof(Nodo));
+    void* elemNodo = malloc(tamElem);
+
+    if(nue == NULL || elemNodo == NULL)
+    {
+        free(nue);
+        free(elemNodo);
+        return NULL;
+    }
+
+    memcpy(elemNodo, elem, tamElem);
+
+    nue->elem= elemNodo;
+    nue->tamElem= tamElem;
+    nue->sig = NULL;
+}
+
+void destruirNodo(Nodo* nae,void* elem,size_t tamElem)
+{
+    memcpy(elem, nae->elem, min(tamElem, nae->tamElem));
+    free(nae->elem);
+    free(nae);
+}
+
